3517c9945f49f4b196117ee144d1245bfb6ee81b
programmerruamkit <programmerruamkit@gmail.com>
2025-01-23 08:52:33 +0700

    test delete comment

--- class/meg_function.php
+++ class/meg_function.php
@@ -9,14 +9,7 @@
 //     return $conn;
 // }
 function connect($db) {
-//$serverName = "203.150.29.241\SQLEXPRESS,1433";
-//$userName = "sa";
-//$userPassword = 'tm$wa01';
-//$dbName = $db;
-//$connectionInfo = array("Database" => $dbName, "UID" => $userName, "PWD" => $userPassword, "MultipleActiveResultSets" => true, "CharacterSet" => 'UTF-8');
-//$conn = sqlsrv_connect($serverName, $connectionInfo);
-//return $conn;
-    //$serverName = "61.91.5.110";
+
     $serverName = "RK01";  
     $userName = "sa";
     $userPassword = 'Fpce#9084';
